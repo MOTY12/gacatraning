f40afcb3822784f2c25627b67111e77bed953346
MOTY <50269513+MOTY12@users.noreply.github.com>
2021-06-01 14:44:49 +0100

    hjuhjyu

--- routes/training.js
+++ routes/training.js
@@ -114,6 +114,22 @@ router.get('/candidatebytraining', async(req, res) => {
 })
 
 
+
+//get new training by id
+router.get('/top/:id', async(req,res)=>{
+        const trainingid = await Training.findById(req.params.id)
+        //.populate('form', 'fieldName')
+        if(!trainingid){
+        res.status(500).json({
+            success: false,
+            message: "No data for the field requested"
+        })
+    }
+        res.status(200).json({data: trainingid})
+    })
+
+
+
 //update training
 router.put('/top/:id', async(req, res) => {
     try {
@@ -151,19 +167,6 @@ router.put('/top/:id', async(req, res) => {
 
 
 
-
-//get new training by id
-router.get('/top/:id', async(req,res)=>{
-        const trainingid = await Training.findById(req.params.id)
-        //.populate('form', 'fieldName')
-        if(!trainingid){
-        res.status(500).json({
-            success: false,
-            message: "No data for the field requested"
-        })
-    }
-        res.status(200).json({data: trainingid})
-    })
 
 
 
